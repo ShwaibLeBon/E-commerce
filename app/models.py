f4e7720c3805923c93bd75fad8d0855fc4839304
AngeFlower <angefleurisseineza@gmail.com>
2021-03-12 09:05:28 +0100

    model produit

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=500)
-    photo = models.ImageField(default='', upload_to='products/')
-    price = models.IntegerField()
+# # Create your models here.
+# class Product(models.Model):
+#     name = models.CharField(max_length=500)
+#     photo = models.ImageField(default='', upload_to='products/')
+#     price = models.IntegerField()
 
