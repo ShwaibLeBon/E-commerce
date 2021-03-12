f3142b14b4cf50640590423c05ce746fa06fa02f
AngeFlower <angefleurisseineza@gmail.com>
2021-03-12 09:07:35 +0100

    user models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	phone_number = models.CharField(max_length=24, default= "+257 000 000")
+	# avatar = models.ImageField(null=True,blank=True, upload_to="profil/avatars/")
+	is_valid = models.BooleanField(default=False)
+	email = models.EmailField()
+	def __str__(self):
+		return f"{self.user.username}-{self.user.email}-{self.is_valid}"
+		
