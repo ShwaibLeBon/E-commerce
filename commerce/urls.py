9ae167670207e697747b698093d5e22dab157ddd
AngeFlower <angefleurisseineza@gmail.com>
2021-03-12 09:06:48 +0100

    urls project

--- commerce/urls.py
+++ commerce/urls.py
@@ -20,9 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    # path('', include('app.urls')),
     path('account/', include('user.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
