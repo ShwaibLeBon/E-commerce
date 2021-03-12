5e96b3ac93f4b33663b80e1628407618f5360818
AngeFlower <angefleurisseineza@gmail.com>
2021-03-12 09:06:20 +0100

    views

--- app/views.py
+++ app/views.py
@@ -1,27 +1,27 @@
-from app.models import Product
-from django.http import request
-from django.shortcuts import render, redirect
-from django.views.generic import *
-from . import models
+# from app.models import Product
+# from django.http import request
+# from django.shortcuts import render, redirect
+# from django.views.generic import *
+# from . import models
 
-# Create your views here.
-def HomeView(request):
-    return render(request, 'app/home.html')
+# # Create your views here.
+# def HomeView(request):
+#     return render(request, 'app/home.html')
 
-def CartView(request):
-    return render(request, 'app/cart.html')
+# def CartView(request):
+#     return render(request, 'app/cart.html')
 
-def ContactView(request):
-    return render(request, 'app/contact.html')
+# def ContactView(request):
+#     return render(request, 'app/contact.html')
 
-def AboutView(request):
-    return render(request, 'app/about.html')
+# def AboutView(request):
+#     return render(request, 'app/about.html')
 
-class ProductView(ListView):
-    model = Product
-    template_name = 'app/product.html'
+# class ProductView(ListView):
+#     model = Product
+#     template_name = 'app/product.html'
     
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'app/productdetails.html'
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = 'app/productdetails.html'
 
