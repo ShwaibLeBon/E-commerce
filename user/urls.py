2b0ef111b2c46119432d2f104485d30b2fd7f890
AngeFlower <angefleurisseineza@gmail.com>
2021-03-12 09:08:24 +0100

    urls user

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('user-profil/',profile,name='profil'),
+    path('user-home/',home,name='home'),
+    path('user-deconnexion/',deconnexion,name='deconnexion'),
 ]
